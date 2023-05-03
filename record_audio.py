717bb9b0b97dc82b059eb1a06e22fd002c6f65e0
bubiche <bubiche95@gmail.com>
2023-05-03 20:50:13 +0800

    slightly more natural start record flow

--- record_audio.py
+++ record_audio.py
@@ -21,13 +21,18 @@ def record_audio(audio_queue, energy_threshold=300, pause_threshold=0.8, dynamic
     r.dynamic_energy_threshold = dynamic_energy_threshold
 
     with sr.Microphone(sample_rate=16000) as source:
-        print("Audio Record Ready")
+        r.adjust_for_ambient_noise(source)
 
+        is_first_time_after_toggling_record = True
         while True:
             if toggle.is_recording:
+                if is_first_time_after_toggling_record:
+                    print("Audio Record Ready")
+                    is_first_time_after_toggling_record = False
                 audio = r.listen(source)
                 torch_audio = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0)
                 audio_data = torch_audio
                 audio_queue.put_nowait(audio_data)
             else:
+                is_first_time_after_toggling_record = True
                 time.sleep(0.5)
