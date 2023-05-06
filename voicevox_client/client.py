77845707e8df8f420574466a879944f404b76b06
bubiche <bubiche95@gmail.com>
2023-05-06 21:52:28 +0800

    simplify vox client

--- voicevox_client/client.py
+++ voicevox_client/client.py
@@ -1,3 +1,4 @@
+# Heavily inspired by https://github.com/voicevox-client/python
 from httpx import AsyncClient
 
 # core_version in the methods below is voicevox core version, check for new ones here: https://github.com/VOICEVOX/voicevox_core/releases
@@ -9,13 +10,16 @@ class Client:
             base_url (string): Voicevox engine base url
             timeout (integer | None): request timeout, useful to prevent hanging in CPU mode
         """
-        self.http_client = HttpClient(base_url=base_url, timeout=timeout)
+        self.http_client = AsyncClient(base_url=base_url, timeout=timeout)
 
     async def fetch_speakers(self, core_version=None):
         """
         Fetch speakers list
         """
-        return await self.http_client.get_speakers(core_version)
+        params = {}
+        if core_version is not None:
+            params["core_version"] = core_version
+        return await self.__request("GET", "/speakers", params=params)
 
     async def fetch_speaker_info(self, speaker_uuid, core_version=None):
         """
@@ -25,13 +29,18 @@ class Client:
             speaker_uuid (string)
             core_version (string | None)
         """
-        return await self.http_client.get_speaker_info(speaker_uuid, core_version)
+        params = {
+            "speaker_uuid": speaker_uuid
+        }
+        if core_version is not None:
+            params["core_version"] = core_version
+        return await self.__request("GET", "/speaker_info", params=params)
 
     async def close(self):
         """
         Close http_client, must be called at the end
         """
-        await self.http_client.close()
+        await self.http_client.aclose()
 
     async def __aenter__(self):
         print("Voicevox Client ENTER")
@@ -41,16 +50,8 @@ class Client:
         print("Voicevox Client EXIT")
         await self.close()
 
-
-class HttpClient:
-    def __init__(self, base_url, timeout):
-        self.session = AsyncClient(base_url=base_url, timeout=timeout)
-
-    async def close(self):
-        await self.session.aclose()
-
-    async def request(self, method, path, **kwargs):
-        response = await self.session.request(method, path, **kwargs)
+    async def __request(self, method, path, **kwargs):
+        response = await self.http_client.request(method, path, **kwargs)
 
         if response.status_code == 200 or response.status_code == 204:
             if response.headers.get("content-type") == "application/json":
@@ -58,22 +59,8 @@ class HttpClient:
             else:
                 return response.content
         else:
-            raise HttpException(response.json())
-
-    async def get_speakers(self, core_version=None):
-        params = {}
-        if core_version is not None:
-            params["core_version"] = core_version
-        return await self.request("GET", "/speakers", params=params)
-
-    async def get_speaker_info(self, speaker_uuid, core_version=None):
-        params = {
-            "speaker_uuid": speaker_uuid
-        }
-        if core_version is not None:
-            params["core_version"] = core_version
-        return await self.request("GET", "/speaker_info", params=params)
+            raise self.HttpException(response.json())
 
 
-class HttpException(Exception):
-    pass
+    class HttpException(Exception):
+        pass
